91f67b5ade66947781d2416ce1fa01cfceac82c1
sh0ur1 <untitled.project17@gmail.com>
2026-07-13 11:21:41 +0700

    start UI

---
+++ main.py
@@ -0,0 +1,9 @@
+from count_up import start_flow_session
+
+
+# -------------------------------------
+def draw_start_menu():
+    program_name = "progressive flowmodoro"
+    start_main_menu = "[1] start flow \n[2] exit program"
+    print(program_name)
+    print(start_main_menu)
